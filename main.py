88c02c885cc5e932e0a32dc08c555ca2ef60cde8
İlker Aydın <saldalilker@gmail.com>
2024-07-24 12:27:53 +0300

    message revision

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from plyer import notification
 
 notification_title = "DRINK WATER!!"
 
-notification_message="you must drink water now bro"
+message =input("notification message: ")
+
+notification_message= message
 
 notification.notify(title=notification_title,message=notification_message,app_icon=None,timeout=10,toast=False)
 
